5fa4af13f44a73fa14179440cbb9a4493098208a
Rainer M Krug <Rainer@krugs.de>
2025-06-11 09:53:32 +0200

    Simplify api_call to exclude error handling and add test

--- R/api_call.R
+++ R/api_call.R
@@ -25,47 +25,47 @@ api_call <- function(req) {
     return(resp)
   }
 
-  # Further processing
-  content_type <- httr2::resp_content_type(resp)
+  # # Further processing
+  # content_type <- httr2::resp_content_type(resp)
 
-  # Parse body as string for inspection
-  resp_string <- httr2::resp_body_string(resp)
+  # # Parse body as string for inspection
+  # resp_string <- httr2::resp_body_string(resp)
 
-  # Special cases handling
-  if (status == 400) {
-    stop("HTTP status 400 Request Line is too large")
-  }
+  # # Special cases handling
+  # if (status == 400) {
+  #   stop("HTTP status 400 Request Line is too large")
+  # }
 
-  if (status == 429) {
-    stop("HTTP status 429 Too Many Requests")
-    # return(empty_res)
-  }
+  # if (status == 429) {
+  #   stop("HTTP status 429 Too Many Requests")
+  #   # return(empty_res)
+  # }
 
-  if (status == 503) {
-    mssg <- regmatches(
-      resp_string,
-      regexpr("(?<=<title>).*?(?=<\\/title>)", resp_string, perl = TRUE)
-    )
-    stop(mssg, ". Please try setting `per_page = 25` in your function call!")
-    # return(empty_res)
-  }
+  # if (status == 503) {
+  #   mssg <- regmatches(
+  #     resp_string,
+  #     regexpr("(?<=<title>).*?(?=<\\/title>)", resp_string, perl = TRUE)
+  #   )
+  #   stop(mssg, ". Please try setting `per_page = 25` in your function call!")
+  #   # return(empty_res)
+  # }
 
-  # Generic error handling
-  if (status >= 400) {
-    parsed <- tryCatch(
-      jsonlite::fromJSON(resp_string, simplifyVector = FALSE),
-      error = function(e) list(error = "Unknown error", message = resp_string)
-    )
-    stop(
-      sprintf(
-        "OpenAlex API request failed [%s]\n%s\n<%s>",
-        status,
-        parsed$error,
-        parsed$message
-      ),
-      call. = FALSE
-    )
-  }
+  # # Generic error handling
+  # if (status >= 400) {
+  #   parsed <- tryCatch(
+  #     jsonlite::fromJSON(resp_string, simplifyVector = FALSE),
+  #     error = function(e) list(error = "Unknown error", message = resp_string)
+  #   )
+  #   stop(
+  #     sprintf(
+  #       "OpenAlex API request failed [%s]\n%s\n<%s>",
+  #       status,
+  #       parsed$error,
+  #       parsed$message
+  #     ),
+  #     call. = FALSE
+  #   )
+  # }
 
   # Any other unexpected status (not 429 or 200)
   stop("HTTP status ", status)
