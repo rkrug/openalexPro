7b9ec0bbc7babd2e76876767f3fa2a12c682e5b7
Rainer M Krug <Rainer@krugs.de>
2025-10-26 13:17:44 +0100

    Update startup message

--- R/zzz.R
+++ R/zzz.R
@@ -2,9 +2,15 @@
   # packageStartupMessage("Thank you for using mypackage! 🚀")
   packageStartupMessage(
     "
-      This package is in alpha phase and will likely change!
+      This package is in beta phase.
 
-      Use at own risk (as usual)!
+      The functions definitions are likely stable in the 
+      release on r-universe https://rkrug.r-universe.dev/openalexPro,
+      but nothing can be assumed in the dev version on GittHub!
+
+      As usual, the author(s) do not take any responsibility for the 
+      correct functioning of the package or any responsibility for 
+      resulting wrong statements , delays, costs, etc.
     "
   )
 }
